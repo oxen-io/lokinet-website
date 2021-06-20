fc94cc9c3dfb148e73663a5e0701ea1424ea310a
Audric Ackermann <audric@loki.network>
2021-06-20 17:04:09 +1000

    move remaining components to styled components

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,12 +1,21 @@
-import styles from "./Header.module.css";
-import Logo from "../logo/Logo";
-import Spacer from "../spacer/Spacer";
+import styled from 'styled-components'
+import Logo from '../logo/Logo'
+import Spacer from '../spacer/Spacer'
+
+const StyledHeader = styled.div`
+  min-height: 100px;
+  align-items: center;
+  display: flex;
+  background-color: lightgrey;
+  flex-grow: 1;
+  width: 100%;
+`
 
 export default function Header() {
   return (
-    <header className={styles.container}>
+    <StyledHeader>
       <Logo />
       <Spacer />
-    </header>
-  );
+    </StyledHeader>
+  )
 }
--- components/spacer/Spacer.tsx
+++ components/spacer/Spacer.tsx
@@ -1,5 +1,7 @@
-import styles from "./Spacer.module.css";
-
+import styled from 'styled-components'
+const StyledSpacer = styled.div`
+  flex-grow: 1;
+`
 export default function Spacer() {
-  return <div className={styles.container} />;
+  return <StyledSpacer />
 }
