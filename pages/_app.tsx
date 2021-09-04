80542f0d8b41d17e8dd5bc65510ca7a67c9f3834
Audric Ackermann <audric@oxen.io>
2021-09-04 09:33:09 +0200

    Fixed theme padding without js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,23 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    
+
+  }
+
+  :root {
+    --margins-xs: 5px;
+    --margins-sm: 10px;
+    --margins-md: 15px;
+    --margins-lg: 20px;
+    --margins-xl: 30px;
+    --margins-xxl: 70px;
+    --duration-linkBottom: 0.1s;
+    --color-text: black;
+    --color-background: white;
+    --color-secondary: rgb(58, 58, 58);
+    --color-border-accordion: rgb(212, 212, 212);
+    --initial-color-mode: 'light';
   }
 `;
 function App({ Component, pageProps }: AppProps) {
