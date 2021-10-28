389bc1d9e89e2062eb7cbac6dafd11fde1c13611
Audric Ackermann <audric@loki.network>
2021-10-28 14:26:45 +1100

    fix url of windows download zip to exe

--- pages/windows.tsx
+++ pages/windows.tsx
@@ -1,46 +1,46 @@
-import { NextApiResponse } from "next"
-
+import { NextApiResponse } from "next";
 
 function Page() {
   // Render data...
 }
 
-export async function getServerSideProps({ res }: {res: NextApiResponse}) {
+export async function getServerSideProps({ res }: { res: NextApiResponse }) {
+  const fetched = await fetch(
+    "https://api.github.com/repos/oxen-io/lokinet/releases/latest"
+  );
 
-  const fetched = await fetch('https://api.github.com/repos/oxen-io/lokinet/releases/latest');
   if (!fetched) {
     return {
       notFound: true,
-    }
+    };
   }
-  const json = await fetched.json()
+  const json = await fetched.json();
   if (!json || !json.assets) {
     return {
       notFound: true,
-    }
+    };
   }
-  const assetsUrl =  json.assets.map((m:any) => m.browser_download_url)
+
+  const assetsUrl = json.assets.map((m: any) => m.browser_download_url);
   if (!assetsUrl || !assetsUrl.length) {
     return {
       notFound: true,
-    }
+    };
   }
-  const windowsAssetUrl = assetsUrl.find((a:string) => a.endsWith('.zip'))
+  const windowsAssetUrl = assetsUrl.find((a: string) => a.endsWith(".exe"));
   if (!windowsAssetUrl) {
     return {
       notFound: true,
-    }
+    };
   }
 
   return {
     redirect: {
       permanent: false,
-      destination:windowsAssetUrl,
+      destination: windowsAssetUrl,
     },
-    props:{},
+    props: {},
   };
 }
-
-
 
 export default Page;
