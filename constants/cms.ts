89aa44230aa9c38d2e9956a989200dab63cbe64b
tomobre <tomobre@gmail.com>
2022-10-07 15:03:26 +1100

    feat: remove environment file and revalidate rate  600 in const

--- constants/cms.ts
+++ constants/cms.ts
@@ -1,9 +1,7 @@
-import isLive from "../utils/environment";
-
 const CMS = {
   // Next.js will try and re-build the page when a request comes in
   // every 10 minutues for production and every 30 seconds for staging
-  CONTENT_REVALIDATE_RATE: isLive() ? 600 : 30,
+  CONTENT_REVALIDATE_RATE: 600,
 };
 
 export default CMS;
--- utils/environment.ts
+++
@@ -1,4 +0,0 @@
-export default function isLive(): boolean {
-  // only show 'live' content in production
-  return process.env.NEXT_PUBLIC_SITE_ENV == "production" ? true : false;
-}
